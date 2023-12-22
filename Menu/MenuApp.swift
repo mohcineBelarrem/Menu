f7e561a9a161dda0315831936d522560dbf33125
Mohcine Belarrem <belarrem.mohcine@gmail.com>
2023-12-22 11:52:55 +0100

    Initial Commit

---
+++ Menu/MenuApp.swift
@@ -0,0 +1,17 @@
+//
+//  MenuApp.swift
+//  Menu
+//
+//  Created by Mohcine on 22/12/2023.
+//
+
+import SwiftUI
+
+@main
+struct MenuApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
