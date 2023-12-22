c09f6ba33251c72c6cd9a93ef88f1b4f1bd1acc0
Mohcine Belarrem <belarrem.mohcine@gmail.com>
2023-12-22 18:23:53 +0100

    Populates Data

--- Menu/Model/MenuItem.swift
+++ Menu/Model/MenuItem.swift
@@ -22,29 +22,56 @@ enum Ingredient: String {
     case lamb
     case driedPrunes
     case friedAlmonds
+    case orange
     case safran
     case sugar
+    case chicken
+    case chickpea
+    case potato
     case honey
     case orangeBlossomWater
+    case mint
+    case coffee
+    case greenTea
+    case semoulina
+    case courgette
+    case pumpkin
+    case eggplant
+    case weirdStuff
+    case water
+    case lemon
+    case almondsPaste
+    case pastillaSheet
+    case fish
+    case onions
+    case vermicelli
+    case ice
+    case strawberry
+    case milk
+    case chocolate
+    case butterMilk
+    case sesameSeeds
     
     var userFriendlyName: String {
         switch self {
-        case .tomato:
-            return "Tomato"
-        case .lamb:
-            return "Lamb"
         case .driedPrunes:
             return "Dried Prunes"
         case .friedAlmonds:
             return "Fried Almonds"
-        case .safran:
-            return "Safran"
-        case .sugar:
-            return "Sugar"
-        case .honey:
-            return "Honey"
         case .orangeBlossomWater:
             return "Orange Blossom Water"
+        case .weirdStuff:
+            return "Preservatifs and stuff like that"
+        case .almondsPaste:
+            return "Alomnds Paste"
+        case .pastillaSheet:
+            return "Pastilla sheet"
+        case .sesameSeeds:
+            return "Sesame seeds"
+        case .butterMilk:
+            return "Butter Milk"
+        default:
+            return self.rawValue.capitalized
         }
     }
 }
